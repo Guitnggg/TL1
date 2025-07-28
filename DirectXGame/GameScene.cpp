5720a4b912194248b6746a7cbcb7dfa3db5df8e2
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-28 16:10:23 +0900

    Tips 20pまで

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -13,9 +13,9 @@ GameScene::~GameScene() {
 }
 
 void GameScene::Initialize() {
-    //========================================
+    //==================================================
     // jsonファイルのデシリアライズ化
-    //========================================
+    //==================================================
 
     // 連結してフルパスを得る
     const std::string fullpath = std::string("Resources/levels/") + "scene.json";
@@ -41,23 +41,52 @@ void GameScene::Initialize() {
     assert(deserialized.contains("name"));     // "name"が含まれているか
     assert(deserialized["name"].is_string());  // ["name"]は文字列か？
 
-    //========================================
+    //==================================================
     // レベルデータを構造体に格納していく
-    //========================================
+    //==================================================
 
     LevelData* levelData = new LevelData();
 
-    // 
+    // "name"を文字列として取得
     levelData->name = deserialized["name"].get < std::string>();
-    assert(levelData->name == "scene");  // 
+    assert(levelData->name == "scene");  // それは"scene"なのか
 
     // "objects"の全オブジェクトの走査
     for (nlohmann::json& object : deserialized["objects"]) {
-        // 
-        assert(object["type"].get<std::string>() == "MESH") {
-            // 
+        // オブジェクト１つ分の妥当性のチェック
+        assert(object.contains("type"));  // "type"が含まれているか
+
+        if (object["type"].get<std::string>() == "MESH") {
+            // １個分の要素の準備
             levelData->objects.emplace_back(ObjectData{});
-            ObjectData& objectData = levelData->objects.back();
+            ObjectData& objectData = levelData->objects.back();  // 追加要素の参照を用意し可読性もよくなる
+
+            objectData.type = object["type"].get<std::string>();  // 
+            objectData.name = object["name"].get<std::string>();  // 
+
+            // トランスフォームのパラメーター読み込み
+            nlohmann::json& transform = object["transform"];
+
+            // 平行移動"transform"
+            objectData.transform.translation.x = (float)transform["translation"][0];
+            objectData.transform.translation.y = (float)transform["translation"][2];
+            objectData.transform.translation.z = (float)transform["translation"][1];
+
+            // 回転角"rotation"
+            objectData.transform.rotation.x = -(float)transform["rotation"][0];
+            objectData.transform.rotation.y = -(float)transform["rotation"][2];
+            objectData.transform.rotation.z = -(float)transform["rotation"][1];
+
+            // 拡大縮小"scaling"
+            objectData.transform.scaling.x = (float)transform["scaling"][0];
+            objectData.transform.scaling.y = (float)transform["scaling"][2];
+            objectData.transform.scaling.z = (float)transform["scaling"][1];
+            
+            // "file_name"
+            if (object.contains("file_name")) {
+                objectData.file_name = object["file_name"].get<std::string>();
+            }
+
         }
     }
 }
