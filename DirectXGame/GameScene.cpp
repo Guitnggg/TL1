2b47748bbed8baec63b7f53ebc0bd5e10a7c5db9
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-28 14:22:32 +0900

    資料8pまで

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,5 +1,8 @@
 #include "GameScene.h"
 
+#include <cassert>
+#include <fstream>
+
 using namespace KamataEngine;
 
 GameScene::~GameScene() {
@@ -7,7 +10,34 @@ GameScene::~GameScene() {
 }
 
 void GameScene::Initialize() {
+    // 連結してフルパスを得る
+    const std::string fullpath = kDefaultBaseDirectory + filename + kExtension;
 
+    // ファイルストリーム
+    std::ifstream file;
+
+    // ファイルを開く
+    file.open(fullpath);
+    // ファイルオープン失敗をチェック
+    if (file.fall()) {
+        assert(0);
+    }
+
+    // JSON文字列から解凍したデータ
+    nlohmann::json deserialized;
+
+    // 解凍
+    file >> deserialized;
+
+    // 正しいレベルデータファイルかチェック
+    assert(deserialized.is_object());
+    assert(deserialized.contains("name"));
+    assert(deserialized["name"].is_string());
+
+    // "name"を文字列として取得
+    std::string name = deserialized["name"].get<std::string>();
+    // 正しいレベルデータファイルかチェック
+    assert(name.compare("scene") == 0);
 }
 
 void GameScene::Update() {
