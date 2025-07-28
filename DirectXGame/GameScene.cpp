d1ae2b792c1e9d73e19db8b38f5f0a457b967b38
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-28 15:11:21 +0900

    一旦

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,5 +1,10 @@
 #include "GameScene.h"
 
+#include <string>
+#include <json.hpp>
+#include <fstream>
+#include <cassert>
+#include <vector>
 
 using namespace KamataEngine;
 
@@ -8,9 +13,18 @@ GameScene::~GameScene() {
 }
 
 void GameScene::Initialize() {
-    
+    // 連結してフルパスを得る
+    const std::string fullpath = std::string("Resources/levels/") + "scene.json";
 
+    // ファイルストリーム
+    std::ifstream file;
 
+    // ファイルを開く
+    file.open(fullpath);
+    // ファイルオープン失敗をチェック
+    if (file.fail()) {
+        assert(0);
+    }
 }
 
 void GameScene::Update() {
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -25,6 +25,39 @@ public:
     /// </summary>
     void Draw();
 
+public:
+
+    /// <summary>
+    /// オブジェクト 1個分のデータ
+    /// </summary>
+    struct ObjectData {
+        std::string type; //"type"
+        std::string name; //"name"
+
+        //"transform"
+        struct Transform {
+            KamataEngine::Vector3 translation; //"translation"
+            KamataEngine::Vector3 rotation;    //"rotation"
+            KamataEngine::Vector3 scaling;     //"scaling"
+        };
+
+        Transform transform; // メンバの準備
+
+        //"file name"
+        std::string file_name;
+    };
+
+    /// <summary>
+    /// レベルデータ
+    /// </summary>
+    struct LevelData {
+        //"name"
+        std::string name;
+
+        //"object"
+        std::vector<ObjectData> objects;
+    };
+
 private:
 
     KamataEngine::Camera camera_;
